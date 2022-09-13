fdf325475f7279a1c7ff0ef765d0b1f2ab93dec6
devlinzhou <devlinzhou@tencent.com>
2022-09-14 00:35:25 +0800

    modify

--- deterministic/deterministic_float.cpp
+++ deterministic/deterministic_float.cpp
@@ -241,9 +241,9 @@ int main()
 {
 	GFloatTest FT(1000000);
 	
-	bool bBase = true;
+	bool bBase = false;
 	bool bTrigonometric = true;
-	bool bTranscendental = true;
+	bool bTranscendental = false;
 
 	if( bBase )
 	{
@@ -259,13 +259,14 @@ int main()
 	if(bTrigonometric)
 	{
 		FT.FunTest("Sin", 0, 3.14f, [&](int i)->float {return sinf(FT.fa[i]); }, [&](int i)->GFloat {return GFloat::Sin(FT.Ga[i]); });
-		FT.FunTest("Cos", -10000.f, 10000.f, [&](int i)->float {return cosf(FT.fa[i]); }, [&](int i)->GFloat {return GFloat::Cos(FT.Ga[i]); });
+		/*FT.FunTest("Cos", -10000.f, 10000.f, [&](int i)->float {return cosf(FT.fa[i]); }, [&](int i)->GFloat {return GFloat::Cos(FT.Ga[i]); });
 		FT.FunTest("Tan", -10000.f, 10000.f, [&](int i)->float {return tanf(FT.fa[i]); }, [&](int i)->GFloat {return GFloat::Tan(FT.Ga[i]); });
 		FT.FunTest("ASin", -1.f, 1.f, [&](int i)->float {return asinf(FT.fa[i]); }, [&](int i)->GFloat {return GFloat::ASin(FT.Ga[i]); });
 		FT.FunTest("ACos", -1.f, 1.f, [&](int i)->float {return acosf(FT.fa[i]); }, [&](int i)->GFloat {return GFloat::ACos(FT.Ga[i]); });
 		FT.FunTest("ATan", -10000.f, 10000.f, [&](int i)->float {return atanf(FT.fa[i]); }, [&](int i)->GFloat {return GFloat::ATan(FT.Ga[i]); });
 		FT.FunTest("ATan(10,x)", -10000.f, 10000.f, [&](int i)->float {return atan2f(10.f, FT.fa[i]); }, [&](int i)->GFloat {return GFloat::ATan2(GFloat(10), FT.Ga[i]); });
 		FT.FunTest("ATan(x,10)", -10000.f, 10000.f, [&](int i)->float {return atan2f(FT.fa[i], 10.f); }, [&](int i)->GFloat {return GFloat::ATan2(FT.Ga[i], GFloat(10)); });
+	*/
 	}
 
 	if( bTranscendental )
--- deterministic/glacier_float.cpp
+++ deterministic/glacier_float.cpp
@@ -90,35 +90,34 @@ GFloat GFloat::Sin(const GFloat value)
 
     GFloat Fraction = TMod - GFloat::Floor( TMod);
 
-    int32_t TWhole = TMod.GetWhole();*/
+    int32_t TWhole = TMod.GetWhole();
 
 	GFloat Fraction = value;
     if(value > Pi_Half())
     {
         Fraction = Pi() - Fraction;
-    }
+    }*/
 
+	int32_t frac = value.getfraction() << 8;
+	int32_t exp = value.getexponent()+22 -127;
+
+	GFixed30 TFrac(frac);
+	float ff= value.toFloat();
+
+	constexpr GFixed30 C_sqrt2(0, 70710678, 100000000);
 	constexpr GFixed30 C_1(1, 0, 10000);
 	constexpr GFixed30 C_1_2(0, 1, 2);	
 	constexpr GFixed30 C_1_6(0, 1, 6);
 	constexpr GFixed30 C_1_24(0, 1, 24);
 	constexpr GFixed30 C_1_120(0, 1, 120);
 
-	constexpr GFixed30 C_sqrt2(0, 707107, 1000000);
 	constexpr GFixed30 C_quaterPi(0, 785398164, 1000000000);
 
-	GFixed30 x1 = GFixed30::FromGFloat(Fraction) - C_quaterPi;
-	GFixed30 x2 = x1 * x1;
-	GFixed30 x3 = x2 * x1;
-	GFixed30 x4 = x3 * x1;
-	GFixed30 x5 = x4 * x1;
+	GFixed30 x1 = GFixed30::FromGFloat(value) - C_quaterPi;
 
 	//GFixed30 SinValue = C_sqrt2 * (C_1 + x1 - C_1_2 * x2 - C_1_6 * x3 + C_1_24 * x4 + C_1_120 * x5 );
 
-	GFixed30 SinValue = C_sqrt2 * (C_1 +  x1 *(C_1 - C_1_2 * x1 - C_1_6 * x2 + C_1_24 * x3 + C_1_120 * x4) );
-
-	//GFixed30 SinValue = C_sqrt2 * (C_1 + x1 * (C_1 - x1 * ( C_1_2 * x1 - C_1_6 * x2 + C_1_24 * x3 + C_1_120 * x4));
-
+	GFixed30 SinValue = C_sqrt2 * (C_1 +  x1 *(C_1 + x1*( - C_1_2 + x1 * (- C_1_6  + x1 * (C_1_24 + C_1_120 * x1)))));
 
   //  if( TWhole % 2 == 1 || TWhole % 2 == -1)
     {
